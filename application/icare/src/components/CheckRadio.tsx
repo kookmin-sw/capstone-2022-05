3aab5e5e9521242c13a4d1c09f35872acf83cbb0
sowish23 <soso2266@naver.com>
2022-03-15 23:13:06 +0900

    check radio componentation

---
+++ application/icare/src/components/CheckRadio.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from "react";
+import { View } from "react-native";
+import styled from 'styled-components/native';
+import { RadioButton } from 'react-native-paper';
+
+const RadioBox = styled.TouchableOpacity`
+    justify-content: center;
+    flex-direction: row;
+    border: 1px solid #AEC4BA;
+    border-radius: 10px;
+    padding: 4px;
+    margin: 12px 0;
+`;
+
+const RadioOption = styled.TouchableOpacity`
+    flex-direction: row;
+`;
+
+const RadioLabel = styled.View`
+    justify-content: center;
+    align-items: center;
+`;
+
+const RadioLabelText = styled.Text`
+    font-size: 16px;
+    margin: 0 24px 0 6px;
+`;
+
+const CheckRadio: FC = (props) => {
+    const [checked, setChecked] = React.useState('parents');
+
+    return (
+        <RadioBox>
+            <RadioOption 
+                onPress={() => setChecked('parents')}>
+                <RadioButton
+                    value="parents"
+                    status={ checked === 'parents' ? 'checked' : 'unchecked' }
+                    onPress={() => setChecked('parents')}
+                    color="#AEC4BA"
+                />
+                <RadioLabel>
+                    <RadioLabelText>부모</RadioLabelText>
+                </RadioLabel>
+            </RadioOption>
+            <RadioOption 
+                onPress={() => setChecked('babysitter')}>
+                <RadioButton
+                    value="babysitter"
+                    status={ checked === 'babysitter' ? 'checked' : 'unchecked' }
+                    onPress={() => setChecked('babysitter')}
+                    color="#AEC4BA"
+                />
+                <RadioLabel>
+                    <RadioLabelText>베이비 시터</RadioLabelText>
+                </RadioLabel>
+            </RadioOption>
+        </RadioBox>
+    );
+};
+export default CheckRadio;
--- application/icare/src/pages/Auth/index.tsx
+++ application/icare/src/pages/Auth/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
-import { StyleSheet, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Image, ScrollView, TouchableOpacity, Text } from "react-native";
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../../RootStackParams';
-import { RadioButton, Text } from 'react-native-paper';
+import CheckRadio from "../../components/CheckRadio"
 import LabelInput from "../../components/LabelInput"
 import LabelButton from "../../components/LabelButton"
 
@@ -11,7 +11,6 @@ type authScreenProp = StackNavigationProp<RootStackParamList, 'Auth'>;
 
 const AuthScreen: FC = () => {
     const navigation = useNavigation<authScreenProp>();
-    const [checked, setChecked] = React.useState('parents');
 
     return (
         <ScrollView style={styles.scrollViewContainer}>
@@ -21,32 +20,7 @@ const AuthScreen: FC = () => {
                 </View>
                 <View style={styles.authContent}>
                     <View style={styles.authRadio}>
-                        <TouchableOpacity 
-                            style={styles.authRadioOption}
-                            onPress={() => setChecked('parents')}>
-                            <RadioButton
-                                value="parents"
-                                status={ checked === 'parents' ? 'checked' : 'unchecked' }
-                                onPress={() => setChecked('parents')}
-                                color="#AEC4BA"
-                            />
-                            <View style={styles.authRadioLabel}>
-                                <Text style={styles.authRadioLabelText}>부모</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity 
-                            style={styles.authRadioOption}
-                            onPress={() => setChecked('babysitter')}>
-                            <RadioButton
-                                value="babysitter"
-                                status={ checked === 'babysitter' ? 'checked' : 'unchecked' }
-                                onPress={() => setChecked('babysitter')}
-                                color="#AEC4BA"
-                            />
-                            <View style={styles.authRadioLabel}>
-                                <Text style={styles.authRadioLabelText}>베이비 시터</Text>
-                            </View>
-                        </TouchableOpacity>
+                        <CheckRadio />
                     </View>
                     <View style={styles.authLabelinput}>
                         <LabelInput label="이메일"/>
@@ -57,7 +31,7 @@ const AuthScreen: FC = () => {
                     </View>
                     <View style={styles.authRegisterLabel}>
                         <Text>계정이 없으신가요?</Text>
-                        <Text style={styles.authRegisterText}>회원가입</Text>
+                        <Text style={styles.authRegisterText} onPress={() => navigation.navigate('Register')}>회원가입</Text>
                     </View>
                 </View>
             </View>
@@ -87,26 +61,7 @@ const styles = StyleSheet.create({
         width: "80%",
     },
     authRadio : {
-        flexDirection: 'row',
-        justifyContent: "center",
-        marginTop: 36,
-        marginBottom: 12,
-        padding: 4,
-        borderWidth: 1,
-        borderRadius: 10,
-        borderColor: "#AEC4BA",
-    },
-    authRadioOption : {
-        flexDirection: 'row',
-    },
-    authRadioLabel : {
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    authRadioLabelText : {
-        fontSize: 16,
-        marginRight: 24,
-        marginLeft: 6,
+        marginTop: 24
     },
     authLabelinput: {
         marginTop: 6,
