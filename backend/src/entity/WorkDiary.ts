605bf3170f8f2f9d6df1b818e198e92df0544553
KMUsungwon <sungwontoto@kookmin.ac.kr>
2022-05-06 12:05:44 +0900

    퇴근일지 응답 관련 코드 최적화

--- backend/src/entity/WorkDiary.ts
+++ backend/src/entity/WorkDiary.ts
@@ -41,7 +41,7 @@ export class WorkDiary extends BaseEntity {
         return await this.createQueryBuilder("work_diary")
             .where("work_diary.mappingId = :mappingId", {mappingId: mappingId})
             .andWhere("DATE_FORMAT(work_diary.createdAt, '%Y-%m-%d') = :date", {date: year + "-" + month + "-" + day})
-            .getOneOrFail();
+            .getOne();
     }
 
     // 캘린더에서 클릭한 날짜에 작성된  퇴근일지 가져오기
@@ -50,6 +50,6 @@ export class WorkDiary extends BaseEntity {
         return await this.createQueryBuilder("work_diary")
             .where("work_diary.mappingId = :mappingId", {mappingId: mappingId})
             .andWhere("DATE_FORMAT(work_diary.createdAt, '%Y-%m-%d') = :date", {date: date})
-            .getOneOrFail();
+            .getOne();
     }
 }
