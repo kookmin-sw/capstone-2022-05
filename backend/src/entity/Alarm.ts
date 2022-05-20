32998f9178682dc48299ca3f0c1cff14f8128bbd
KMUsungwon <sungwontoto@kookmin.ac.kr>
2022-05-20 18:23:32 +0900

    hotifx: 날짜별 퇴근일지 기능 수정

--- backend/src/entity/Alarm.ts
+++ backend/src/entity/Alarm.ts
@@ -27,5 +27,27 @@ export class Alarm extends BaseEntity {
     @JoinColumn({name: "mappingId"})
     mapping: Mapping
 
+    // 당일 작성된 알람 이미지 리스트 가져오기
+    static async findAlarmbyMappingId(mappingId: number) {
+
+        const date = new Date();
+        let year: number = date.getFullYear();
+        let month: string = ("0" + (1 + date.getMonth())).slice(-2);
+        let day: string = ("0" + date.getDate()).slice(-2);
+
+        return await this.createQueryBuilder("alarm")
+            .where("alarm.mappingId = :mappingId", {mappingId: mappingId})
+            .andWhere("DATE_FORMAT(alarm.createdAt, '%Y-%m-%d') = :date", {date: year + "-" + month + "-" + day})
+            .getMany();
+    }
+
+    // 알람 테이블에서 클릭한 날짜에 저장된 이미지 가져오기
+    static async findCalendarAlarmList(mappingId: number, date: string) {
+
+        return await this.createQueryBuilder("alarm")
+            .where("alarm.mappingId = :mappingId", {mappingId: mappingId})
+            .andWhere("DATE_FORMAT(alarm.createdAt, '%Y-%m-%d') = :date", {date: date})
+            .getMany();
+    }
 
 }
