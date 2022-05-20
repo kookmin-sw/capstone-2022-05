80038343fd61c8efa90af0f2188b00ef1073d7e1
KMUsungwon <sungwontoto@kookmin.ac.kr>
2022-05-20 14:06:19 +0900

    add feature: 모든 API 토큰 인증 추가

--- backend/src/routes/bs.route.ts
+++ backend/src/routes/bs.route.ts
@@ -3,20 +3,21 @@ import BSController from '../controllers/bs.controller';
 import mappingController from '../controllers/mapping.controller';
 import alarmController from "../controllers/alarm.controller";
 import workdiaryController from "../controllers/workdiary.controller"
+import Authorization from '../middleware/authMiddleware';
 
 
 const router = express.Router();
 
-router.post('/info/:userId', BSController.inputBSInfo);
-router.get('/info/:bsId', BSController.getBSInfo);
-router.patch('/info/:bsId', BSController.updateBSInfo);
+router.post('/info/:userId', Authorization, BSController.inputBSInfo);
+router.get('/info/:bsId', Authorization, BSController.getBSInfo);
+router.patch('/info/:bsId', Authorization, BSController.updateBSInfo);
 
-router.post('/mapping/:bsId', BSController.mappingRequest);
-router.get('/mapping/:bsId', mappingController.findParentList)
+router.post('/mapping/:bsId', Authorization, BSController.mappingRequest);
+router.get('/mapping/:bsId', Authorization, mappingController.findParentList)
 
-router.post('/alarm/:mappingId',  alarmController.upload.single("img"), alarmController.sendAlarm);
+router.post('/alarm/:mappingId', Authorization, alarmController.upload.single("img"), alarmController.sendAlarm);
 
-router.post('/diary/:mappingId', workdiaryController.writeWorkdiary) //,workdiaryController.upload.array("img")
+router.post('/diary/:mappingId', Authorization, workdiaryController.writeWorkdiary) //,workdiaryController.upload.array("img")
 
 router.get('/test',(req,res) => res.send("test1"))
 export = router;
