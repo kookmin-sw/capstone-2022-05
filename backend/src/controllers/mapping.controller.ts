d3bb8bcceff3f0962f1cdf92c649fb4afdd0f547
Youngho <cyh6099@gmail.com>
2022-05-21 16:06:46 +0900

    findParentList수정

--- backend/src/controllers/mapping.controller.ts
+++ backend/src/controllers/mapping.controller.ts
@@ -6,7 +6,7 @@ const findParentList = async (req:Request, res: Response, next: NextFunction) =>
     try{
         const bsId : number = +req.params.bsId; 
     
-        await BabySitter.findById(bsId)
+        // await BabySitter.findById(bsId)
         const result = await Mapping.findMappingParentList(bsId);
 
         const returnData = []
@@ -17,7 +17,7 @@ const findParentList = async (req:Request, res: Response, next: NextFunction) =>
 
         result.map((value: any, index: number) => {
             console.log(typeof(value))
-            returnData.push(value.parent)
+            returnData.push(value)
         })
 
         res.status(200).json(returnData)
