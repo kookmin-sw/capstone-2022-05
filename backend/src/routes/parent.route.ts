babd059c4fc69416091ac72403a764ba7160db3b
KMUsungwon <sungwontoto@kookmin.ac.kr>
2022-05-21 18:05:03 +0900

    코드 리팩토링

--- backend/src/routes/parent.route.ts
+++ backend/src/routes/parent.route.ts
@@ -4,16 +4,16 @@ import Authorization from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-router.post('/info/:id', Authorization, parentController.createParentInfo);
-router.get('/info/:parentId', Authorization, parentController.getParentInfo);
-router.patch('/info/:parentId', Authorization, parentController.editParentInfo);
-router.get('/main/:parentId', Authorization, parentController.getMainPage);
-router.patch('/mapping/acceptance/:mappingId', Authorization, parentController.acceptMapping);
-router.delete('/mapping/rejection/:mappingId', Authorization, parentController.rejectMapping);
-router.get('/diary/:mappingId', Authorization, parentController.getDailyDiary);
-router.post('/calendar/:mappingId', Authorization, parentController.getCalendarDiary);
-router.get('/sensor', Authorization, parentController.getSensorInfo);
-router.patch('/sensor', Authorization, parentController.updateSensorInfo);
+router.post('/info/:id', parentController.createParentInfo);
+router.get('/info/:parentId', parentController.getParentInfo);
+router.patch('/info/:parentId', parentController.editParentInfo);
+router.get('/main/:parentId', parentController.getMainPage);
+router.patch('/mapping/acceptance/:mappingId', parentController.acceptMapping);
+router.delete('/mapping/rejection/:mappingId', parentController.rejectMapping);
+router.get('/diary/:mappingId', parentController.getDailyDiary);
+router.post('/calendar/:mappingId', parentController.getCalendarDiary);
+router.get('/sensor', parentController.getSensorInfo);
+router.patch('/sensor', parentController.updateSensorInfo);
 router.patch('/sensor/alert', parentController.updateSensorAlert);
 
 export = router;
