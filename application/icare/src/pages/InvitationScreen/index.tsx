8d9bd7587b37f713b3a9e4eee3dd823fa71bae6c
sowish23 <soso2266@naver.com>
2022-05-21 22:20:35 +0900

    modify invitation btn

--- application/icare/src/pages/InvitationScreen/index.tsx
+++ application/icare/src/pages/InvitationScreen/index.tsx
@@ -1,5 +1,6 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {FC, useState} from 'react';
+import styled from 'styled-components/native';
 import {View, Text, Image} from "react-native";
 import {StackNavigationProp} from '@react-navigation/stack';
 import { RootStackParamList } from '../../RootStackParams';
@@ -23,10 +24,27 @@ const InvitationScreen: FC  = (props) => {
       <View style={{width: '80%'}}>
         <LabelInput label="초대코드를 입력해주세요" function_state={email} function={setEmail}/>
       </View>
-      <style.LabelComponent onPress={requestMapping}>
-        <LabelButton label="입력완료" />
-      </style.LabelComponent>
+      <LabelBtn color="#AEC4BA" onPress={() => {requestMapping(); navigation.navigate('BSMain')}}>
+          <LabelBtnText>초대하기</LabelBtnText>
+      </LabelBtn>
     </style.Container>
   )
 };
 export default InvitationScreen;
+
+const LabelBtn = styled.TouchableOpacity<{color: string}>`
+    justify-content: center;
+    align-items: center;
+    background-color: ${props => props.color || "#AEC4BA"};
+    padding: 16px;
+    border-radius: 10px;
+    box-shadow: 0 2px rgba(0, 0, 0, .1);
+`;
+
+const LabelBtnText = styled.Text`
+    justify-content: center;
+    align-items: center;
+    color: #FFFFFF;
+    font-size: 16px;
+    font-weight: bold;
+`;
